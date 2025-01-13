5b6abad62fd61f1ff458b50aef9c335ef869903c
“Student” <Student>
2025-01-13 22:58:43 +0300

    UPD "Добавлен пример работы с ошибкой"

--- Less_2/Task_1/main.cpp
+++ Less_2/Task_1/main.cpp
@@ -10,12 +10,24 @@ int main(int argc, char *argv[])
     QNetworkAccessManager networkManager;
     qDebug() << "Network access manager created";
 
+    // Пример когда все работает
+
     QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
     db.setDatabaseName("testdb");
+
+    //Пример когда не все работает
+
+//    QSqlDatabase db = QSqlDatabase::addDatabase("QPSQL");
+//    db.setHostName("127.0.0.1");
+//    db.setDatabaseName("testdb");
+//    db.setPort(5432);
+//    db.setUserName("user");
+//    db.setPassword("12345678");
 
     if(!db.open())
     {
         qDebug() << "Error opening database: " << db.lastError().text();
+        return a.closingDown(); //закрой если все плохо
     } else {
         qDebug() << "Database opened successfully";
     }
